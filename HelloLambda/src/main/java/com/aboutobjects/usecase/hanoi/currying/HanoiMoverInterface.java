25bd641075bb7e61f15f5a044a43f2286d1eb898
aojgarnero <jorge.garnero@aboutobjects.com>
2024-10-21 15:47:22 -0400

    Delete HelloLambda directory

--- HelloLambda/src/main/java/com/aboutobjects/usecase/hanoi/currying/HanoiMoverInterface.java
+++
@@ -1,8 +0,0 @@
-package com.aboutobjects.usecase.hanoi.currying;
-
-import com.aboutobjects.usecase.hanoi.recursion.HanoiTowers;
-
-@FunctionalInterface
-interface HanoiMoverInterface {
-    HanoiTowers move (String operation, HanoiTowers operand);
-}
